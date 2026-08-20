4c73f09b1b0fa3d7f5522805e9e0e38dc4028547
Goutham S <s.gouthama9@gmail.com>
2026-08-20 11:25:21 +0530

    Time: 2 ms (66.78%), Space: 13.9 MB (80.07%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        r=0
+        for i in nums:
+            r^=i
+        return r
+            
+            
+            
+        
+
+        
